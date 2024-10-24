e5f3cd24edaa0ae8ba855ebf4250abaf835eaf0f
muha <Mohamed.Abdulle@metropolia.fi>
2024-10-24 10:16:54 +0300

    valmis

---
+++ src/test/java/LocalizedGreetingTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LocalizedGreetingTest {
+
+    @Test
+    public void testEnglishGreeting() {
+        Locale locale = new Locale("en", "UK");
+        ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("Hello", greeting);
+    }
+
+    @Test
+    public void testSpanishGreeting() {
+        Locale locale = new Locale("es", "ES");
+        ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("Hola", greeting);
+    }
+
+    @Test
+    public void testFrenchGreeting() {
+        Locale locale = new Locale("fr", "FR");
+        ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("Bonjour", greeting);
+    }
+    @Test
+    public void testFarsiGreeting() {
+        Locale locale = new Locale("fa", "IR");
+        ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("سلام! به برنامه ما خوش آمدید", greeting);
+    }
+
+    @Test
+    public void testJapaneseGreeting() {
+        Locale locale = new Locale("ja", "JP");
+        ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("こんにちは", greeting);
+    }
+
+    @Test
+    public void testInvalidChoiceDefaultsToEnglish() {
+        Locale locale = new Locale("en", "UK"); // Simulate default choice
+        ResourceBundle messages = ResourceBundle.getBundle("Message", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("Hello", greeting); // Expected default is English
+    }
+}
